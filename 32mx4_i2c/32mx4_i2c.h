344ea9d8cf9ed41ae808e693823866a54e786165
imperial.dalek42@gmail.com <imperial.dalek42@gmail.com@8bbe6f1c-b789-5162-a2a6-bc72c77501e9>
2011-04-19 17:51:36 +0000

    updated

--- 32mx4_i2c/32mx4_i2c.h
+++ 32mx4_i2c/32mx4_i2c.h
@@ -1,20 +1,30 @@
+#ifndef _32MX4_I2C_H
+	#define _32MX4_I2C_H
+	
+	
 
+	
+	// The 32mx4 has a 8000000 mhz clock
+	#define GetPeripheralClock() 8000000
 
-#define GetPeripheralClock() 8000000
-#define PmodCLS_addr 0x48
-#define EEPROM 0x50
+	
+	// EEPROM Slave Address
+	#define EEPROM_slave_addr 0x50
 
-
-int I2C_Initialize( I2C_MODULE id, UINT32 i2cClock, UINT16 address );
-
-void print_i2c_status( I2C_MODULE id);
-
-void send(I2C_MODULE id, UINT8 byte);
-
-UINT8 eeprom_write_current_byte(I2C_MODULE id, UINT8 slaveAddr, UINT8 byte);
-
-UINT8  read_I2C(I2C_MODULE id, UINT8 slaveAddr);
-
-UINT8 get( I2C_MODULE id);
-
-void  write_I2C(I2C_MODULE id, UINT8 slaveAddr, UINT8 data );
+	
+	int I2C_Initialize( I2C_MODULE id, UINT32 i2cClock, UINT16 address );
+	
+	void print_i2c_status( I2C_MODULE id);
+	
+	UINT8 send(I2C_MODULE id, UINT8 byte);
+	
+	UINT8 eeprom_write_current_byte(I2C_MODULE id, UINT8 slaveAddr, UINT8 byte);
+UINT8 eeprom_sequential_write_page(I2C_MODULE id, UINT8 slaveAddr,UINT8 highAddr, UINT8 lowAddr, UINT8 count, UINT8 buffer[]);
+	UINT8 eeprom_sequential_read_current(I2C_MODULE id, UINT8 slaveAddr, UINT8 count, UINT8 buffer[]);
+	UINT8 read_I2C(I2C_MODULE id, UINT8 slaveAddr);
+	UINT8 sendWithAck(I2C_MODULE id, UINT8 byte);
+	UINT8 get( I2C_MODULE id);
+	UINT8 eeprom_wait_4_ready( I2C_MODULE id, UINT8 slaveAddr);
+int eeprom_write_address( I2C_MODULE id, UINT8 slaveAddr, UINT8 highAddr, UINT8 lowAddr, UINT8 data);
+	void  write_I2C(I2C_MODULE id, UINT8 slaveAddr, UINT8 data );
+#endif
