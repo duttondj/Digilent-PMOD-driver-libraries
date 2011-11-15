ed6f5f0a4ac12cec94952e26a98586ddd0d69c85
captnignit <captnignit@8bbe6f1c-b789-5162-a2a6-bc72c77501e9>
2011-11-15 20:13:47 +0000

    BufLib Changes, Updated heap value in build options, added path to build options

--- PmodLib/PmodCommon/bufferlib/bufferlib.h
+++ PmodLib/PmodCommon/bufferlib/bufferlib.h
@@ -35,10 +35,47 @@ typedef struct
 /* ------------------------------------------------------------ */
 /*				Forward Declarations							*/
 /* ------------------------------------------------------------ */
-void BufLibInitBuffers();
 
-uint16_t BufLibReadBuffer();
+/*  BufLibInitBuffers
+**
+**	Synopsis:
+**  Initailzes all the buffers needed for the AD/DA library
+**
+**  Input: uint8_t BufCount - the number of buffers to create
+**		   uint16_t BufSize - the size of each buffer in bytes
+**
+**  Returns: 0 if successfully created the buffers, 1 if failed to create them
+**
+**	Errors:	1 is returend if failed to create buffers
+*/
+uint8_t BufLibInitBuffers(uint8_t BufCount, uint16_t BufSize);
 
-void BufLibWriteBuffer(uint16_t data);
+/*  BufLibReadBuffer
+**
+**	Synopsis:
+**  Reads a 2-byte value from the buffer, switching to the next if no
+**	data is available in the current
+**
+**  Input: uint16_t pointer - a value for the read 2-bytes to be placed in
+**
+**  Returns: 0 if failed to read 2-bytes, 1 if read 2-bytes
+**
+**	Errors:	returns 0 if failed to read 2-bytes
+*/
+uint8_t BufLibReadBuffer(uint16_t *data);
+
+/*  BufLibWriteBuffer
+**
+**	Synopsis:
+**  Writes a 2-byte value from the buffer, switching to the next if no
+**	room is available in the current
+**
+**  Input: uint16_t - a 2-bytes value to be placed in the buffer
+**
+**  Returns: 0 if failed to write, 1 if wrote 2-bytes
+**
+**	Errors:	returns 0 if failed to write
+*/
+uint8_t BufLibWriteBuffer(uint16_t data);
 
 #endif
--- PmodLib/TestHarness/BuffLib/bufflib_test_driver.c
+++ PmodLib/TestHarness/BuffLib/bufflib_test_driver.c
@@ -21,8 +21,73 @@
 /*				Include File Definitions						*/
 /* ------------------------------------------------------------ */
 
-#include "../../TestHarness/BuffLib/bufflib_test_driver.h"
+#include "./TestHarness/BuffLib/bufflib_test_driver.h"
 
 /* ------------------------------------------------------------ */
 /*				Procedure Definitions							*/
 /* ------------------------------------------------------------ */
+uint8_t UNIT_BufLibRead(UART_MODULE uartID)
+{
+	uint16_t value;
+	uint8_t result = 0;
+
+	if(BufLibWriteBuffer(5))
+	{
+		UARTPutS("Successfully Wrote to Buffer\r\n", uartID);
+	}
+	else
+	{
+		UARTPutS("Write was unsuccessful\r\n", uartID);
+	}
+
+	if(BufLibReadBuffer(&value))
+	{
+		UARTPutS("Successfully read from Buffer\r\n", uartID);
+	}
+	else
+	{
+		UARTPutS("Read was unsuccessful\r\n", uartID);
+	}
+
+	if(value == 5)
+	{
+		result = 1;
+	}
+
+	return result;
+}
+
+uint8_t UNIT_BufLibWrite(UART_MODULE uartID)
+{
+	return 0;
+}
+
+uint8_t UNIT_BufLibReadOverflow(UART_MODULE uartID)
+{
+	return 0;
+}
+
+uint8_t UNIT_BufLibWriteOverflow(UART_MODULE uartID)
+{
+	return 0;
+}
+
+uint8_t UNIT_BufLibInvalidRead(UART_MODULE uartID)
+{
+	return 0;
+}
+
+uint8_t UNIT_BufLibInvalidWrite(UART_MODULE uartID)
+{
+	return 0;
+}
+
+void BufLib_INIT(UART_MODULE uartID)
+{
+	if(!BufLibInitBuffers(3, 6))
+	{
+		UARTPutS("Failed to initalize buffers\r\n", uartID);
+	}
+
+	UARTPutS("Buffers Initialized\r\n", uartID);
+}
